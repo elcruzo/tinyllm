14c96e265289c8a008afaf973a503cde1c522f80
Ayomide Adekoya <ayomideadekoya266@gmail.com>
2023-09-16 21:45:00 -0700

    add n_layers and n_heads to Config

--- include/tinyllm.h
+++ include/tinyllm.h
@@ -7,6 +7,8 @@
 typedef struct {
     int dim;        // transformer dimension
     int hidden_dim; // ffn hidden dimension
+    int n_layers;   // number of transformer layers
+    int n_heads;    // number of attention heads
 } Config;
 
 #endif
