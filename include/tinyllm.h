2ae96ba4f10e784524e32b09d3d3b5ca52a4312d
Ayomide Adekoya <ayomideadekoya266@gmail.com>
2023-09-18 02:20:00 -0700

    add kv_heads, vocab_size, seq_len to Config

--- include/tinyllm.h
+++ include/tinyllm.h
@@ -9,6 +9,9 @@ typedef struct {
     int hidden_dim; // ffn hidden dimension
     int n_layers;   // number of transformer layers
     int n_heads;    // number of attention heads
+    int n_kv_heads; // number of kv heads (for GQA)
+    int vocab_size; // vocabulary size
+    int seq_len;    // max sequence length
 } Config;
 
 #endif
