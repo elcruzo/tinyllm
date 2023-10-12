dd8880fb45ecee1539376ae2fd469343ee65ec55
Ayomide Adekoya <ayomideadekoya266@gmail.com>
2023-10-11 22:30:00 -0700

    implement rmsnorm function

--- src/tinyllm.c
+++ src/tinyllm.c
@@ -2,3 +2,19 @@
 #include <stdlib.h>
 #include <math.h>
 #include "../include/tinyllm.h"
+
+// RMS normalization (used instead of LayerNorm in LLaMA)
+void rmsnorm(float* o, float* x, float* weight, int size) {
+    // calculate sum of squares
+    float ss = 0.0f;
+    for (int j = 0; j < size; j++) {
+        ss += x[j] * x[j];
+    }
+    ss /= size;
+    ss += 1e-5f;
+    ss = 1.0f / sqrtf(ss);
+    // normalize and scale
+    for (int j = 0; j < size; j++) {
+        o[j] = weight[j] * (ss * x[j]);
+    }
+}
